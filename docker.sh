41dfcf806fefa89797384920a98799421afdfd52
Rob Whitby <rob.whitby@springer.com>
2018-07-28 01:49:30 +0100

    log which image is being loaded

--- docker.sh
+++ docker.sh
@@ -6,6 +6,7 @@ cache="$(echo $PWD | cut -d/ -f1-4)/docker-images"
 
 if [ -d "${cache}" ]; then
   for image in ${cache}/*/ ; do
+    echo "Loading $(cat "${image}repository"):$(cat "${image}tag") .."
     docker load -i "${image}image"
     docker tag \
       "$(cat "${image}image-id")" \
