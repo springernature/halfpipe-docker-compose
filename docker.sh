6bcc4ff988805e5684066190d8f756e2a065b7e4
Rob Whitby <rob.whitby@springer.com>
2018-07-28 01:07:09 +0100

    look for "docker-images" dir

--- docker.sh
+++ docker.sh
@@ -4,13 +4,15 @@ set -e
 source /docker-lib.sh
 start_docker
 
-while read -r image; do
-  # Load image
-  docker load -i "${image}/image"
-  # Tag image
-  docker tag \
-    "$(cat "${image}/image-id")" \
-    "$(cat "${image}/repository"):$(cat "${image}/tag")"
-done < <(find /images/* -type d)
+cache="$(echo $PWD | cut -d/ -f1-4)/docker-images"
+
+if [ -d "${cache}s" ]; then
+  for image in ${cache}/*/ ; do
+    echo docker load -i "${image}image"
+    echo docker tag \
+      "$(cat "${image}image-id")" \
+      "$(cat "${image}repository"):$(cat "${image}tag")"
+  done
+fi
 
 exec bash -ec "$@"
