a45c6cf280f2fa785ca52e0a1de04df4dba18bbc
Rob Whitby <rob.whitby@springer.com>
2021-09-15 21:16:43 +0100

    first pass at adding covenant

---
+++ bin/covenant.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+_COVENANT_URL="https://covenant.springernature.app"
+
+source colours.sh
+
+is_covenant_available() {
+    if curl -m 1 -Ls "$_COVENANT_URL" >/dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+sanitise_cdc_name() {
+    echo "$1" | rev | cut -d/ -f1 | rev
+}
+
+cdc_has_been_successful() {
+    local CDC_PROVIDER_NAME="$(sanitise_cdc_name "${1}")"
+    local CDC_PROVIDER_VERSION="${2}"
+    local CDC_CONSUMER_NAME="$(sanitise_cdc_name "${3}")"
+    local CDC_CONSUMER_VERSION="${4}"
+
+    if ! is_covenant_available; then
+        echo -e "${LIGHT_RED}* Covenant is not available; skipping Covenant check${NC}"
+        return 1
+    else
+        local RESULT
+        echo -e "${GREEN}* Checking CDC for consumer ${BLUE}${CDC_CONSUMER_NAME}${GREEN}@${BLUE}${CDC_CONSUMER_VERSION}${GREEN} of provider ${BLUE}${CDC_PROVIDER_NAME}${GREEN}@${BLUE}${CDC_PROVIDER_VERSION}${NC}"
+        RESULT=$(curl -Ls "$_COVENANT_URL/api/v1/result/$CDC_PROVIDER_NAME/$CDC_PROVIDER_VERSION/$CDC_CONSUMER_NAME/$CDC_CONSUMER_VERSION")
+        if [[ "$RESULT" = "PASS" ]]; then
+            echo -e "${GREEN}* Covenant reports a pass for the given versions - skipping execution${NC}"
+            return 0
+        else
+            echo -e "${GREEN}* Covenant reports ${BLUE}$RESULT${GREEN} for the given versions - executing now${NC}"
+            return 1
+        fi
+    fi
+}
+
+record_cdc_result() {
+    local CDC_PROVIDER_NAME="$(sanitise_cdc_name "${1}")"
+    local CDC_PROVIDER_VERSION="${2}"
+    local CDC_CONSUMER_NAME="$(sanitise_cdc_name "${3}")"
+    local CDC_CONSUMER_VERSION="${4}"
+    local RESULT="${5}"
+
+    if ! is_covenant_available; then
+        echo -e "${LIGHT_RED}* Covenant is not available; skipping Covenant update${NC}"
+    else
+        echo -e "${GREEN}* This CDC has completed with result ${BLUE}$RESULT${GREEN} - updating Covenant${NC}"
+        echo curl -X POST -Ls "$_COVENANT_URL/api/v1/result/$CDC_PROVIDER_NAME/$CDC_PROVIDER_VERSION/$CDC_CONSUMER_NAME/$CDC_CONSUMER_VERSION" -d "$RESULT"
+    fi
+}
--- bin/run-cdc.sh
+++ bin/run-cdc.sh
@@ -1,20 +1,40 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Running cdc script"
+source covenant.sh
+
+use_covenant() {
+  [[ "${USE_COVENANT:-}" == "true" ]] && return 0 || return 1
+}
+
+echo "Running CDC. Docs: <https://ee.public.springernature.app/rel-eng/halfpipe/legacy-cdcs/>"
+
+# get current revision of consumer, revert to HEAD if not found
+REVISION=$(curl -m5 -fsSL "${CONSUMER_HOST}/internal/version" | jq -r '.revision' || echo "")
+if [ "${REVISION}" == "" ]; then
+  echo "Error fetching version of consumer from ${CONSUMER_HOST}/internal/version - using HEAD instead."
+  REVISION=HEAD
+  USE_COVENANT="false"
+fi
+
+echo "provider name: ${PROVIDER_NAME}"
+echo "provider version: ${GIT_REVISION}"
+echo "consumer name: ${CONSUMER_NAME}"
+echo "consumer host: ${CONSUMER_HOST}"
+echo "consumer version: ${REVISION}"
+
+# return now if cdc has already been successful
+if use_covenant; then
+  cdc_has_been_successful "${PROVIDER_NAME}" "${GIT_REVISION}" "${CONSUMER_NAME}" "${REVISION}" && return 0
+else
+  echo "covenant disabled"
+fi
+
+echo
 
 # write git key to file
 echo "${CONSUMER_GIT_KEY}" > .gitkey
 chmod 600 .gitkey
-
-set -x
-
-# get current revision of consumer, revert to HEAD if not found
-REVISION=$(curl -fsSL "${CONSUMER_HOST}/internal/version" | jq -r '.revision' || echo "")
-if [ "${REVISION}" == "" ]; then
-  echo "Error fetching version of consumer from ${CONSUMER_HOST}/internal/version - using HEAD instead."
-  REVISION=HEAD
-fi
 
 # clone consumer into /scratch/consumer. dir may already exist when concourse restarts task
 rm -rf /scratch/consumer
@@ -25,8 +45,10 @@ cd /scratch/consumer
 git checkout ${REVISION}
 cd /scratch/consumer/${CONSUMER_PATH}
 
+# continue on error from this point so can record it
+set +e
+
 # run the tests with docker-compose
-# note: old system reads CF manifest env vars and sets them all here
 docker-compose pull ${DOCKER_COMPOSE_SERVICE:-code}
 docker-compose run --no-deps \
   --entrypoint "${CONSUMER_SCRIPT}" \
@@ -36,6 +58,19 @@ docker-compose run --no-deps \
   -e CDC_CONSUMER_VERSION=${REVISION} \
   -e CDC_PROVIDER_NAME=${PROVIDER_NAME} \
   -e CDC_PROVIDER_VERSION=${GIT_REVISION} \
-  ${ENV_OPTIONS} \
-  ${VOLUME_OPTIONS} \
+  ${ENV_OPTIONS:-} \
+  ${VOLUME_OPTIONS:-} \
   ${DOCKER_COMPOSE_SERVICE:-code}
+
+DC_STATUS=$?
+
+echo
+
+# record result in covenant
+if use_covenant; then
+  RESULT="FAIL"
+  [[ $DC_STATUS == 0 ]] && RESULT="PASS"
+  record_cdc_result "${PROVIDER_NAME}" "${GIT_REVISION}" "${CONSUMER_NAME}" "${REVISION}" "${RESULT}"
+fi
+
+exit $DC_STATUS
