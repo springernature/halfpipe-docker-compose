2d0165e745e6cd7419a806f1137c5b49c66f54ce
Rob Whitby <rob.whitby@springer.com>
2023-05-04 12:38:32 +0100

    Revert "skip test"
    This reverts commit 406a48de5b64efa32c715c654c5f006cda753823.

--- integration/03_backward_compatibility/test.sh
+++ integration/03_backward_compatibility/test.sh
@@ -5,7 +5,7 @@ if [ ! -f /halfpipe-shared-cache/cached_file ]; then
     # first time execeution? create the fucking file
     touch /halfpipe-shared-cache/cached_file
     (>&2 echo "Failed to find cached_file on nfs volume")
-    exit 0
+    exit 1
 fi
 
 (>&2 echo "Yea found cached_file on deprecated nfs volume")
